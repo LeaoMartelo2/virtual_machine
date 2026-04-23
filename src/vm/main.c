737f6be83ee40b0ade81659d0ac5c83ecca9c0ce
LeaoMartelo <leaomartelo@gmail.com>
2026-04-23 15:49:20 -0300

    interpreter never actually checked for the -h, -help flags

--- src/vm/main.c
+++ src/vm/main.c
@@ -48,6 +48,8 @@ int main(int argc, char **argv) {
 
     for (int i = 1; i <argc; ++i) {
 
+        if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-help") == 0) { print_usage(argc, argv); }
+
         if(strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "-verbose") == 0) { forced_verbose = true;}
 
         if(strcmp(argv[i], "-md") == 0 || strcmp(argv[i], "-memdump") == 0) { dump_ram = true;}
